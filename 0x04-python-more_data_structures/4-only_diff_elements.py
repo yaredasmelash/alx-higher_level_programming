febbbb3508289183e4d9b0b07a050d1a8003928c
Yared Asmelash <yaredasmelash00@gmail.com>
2024-01-11 00:13:21 +0300

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1 ^ set_2
+    # Create a set that is the union of set_1 and set_2
+    union_set = set_1 | set_2
+
+    # Create a set that the intersection of set_1 and set_2
+    inter_set = set_1 & set_2
+
+    # Subtract the intersection set from the union set to get the set of elements
+    # that are present in only one of the two sets
+    only_diff_set = union_set - inter_set
+
+    return only_diff_set
