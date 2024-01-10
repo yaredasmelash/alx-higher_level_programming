674d63d921cb64c819e40252b0445005e9fe8d6a
Yared Asmelash <yaredasmelash00@gmail.com>
2024-01-11 00:17:52 +0300

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # Create a set that is the union of set_1 and set_2
-    union_set = set_1 | set_2
+    # Get the difference between the two sets
+    diff_set = set_1.symmetric_difference(set_2)
 
-    # Create a set that the intersection of set_1 and set_2
-    inter_set = set_1 & set_2
+    return diff_set
 
-    # Subtract the intersection set from the union set to get the set of elements
-    # that are present in only one of the two sets
-    only_diff_set = union_set - inter_set
-
-    return only_diff_set
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
