733832cb0c075fabb7d8fb1702038782728eb96a
Yared Asmelash <yaredasmelash00@gmail.com>
2024-01-11 00:30:30 +0300

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key, None)
+def simple_delete(a_dictionary, keys=None):
+    if keys:
+        for key in keys:
+            if key in a_dictionary:
+                del a_dictionary[key]
     return a_dictionary
