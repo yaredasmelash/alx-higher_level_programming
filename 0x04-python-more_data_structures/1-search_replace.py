45822d3b04d1aa050014513915c82e0da218a5e9
Yared Asmelash <yaredasmelash00@gmail.com>
2024-01-11 00:08:58 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if num == search else num for num in my_list]
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
