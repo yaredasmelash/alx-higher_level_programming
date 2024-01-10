4ba68c5c8cfa25a198f2bf39d0b661d2f3bad628
Yared Asmelash <yaredasmelash00@gmail.com>
2024-01-11 00:25:02 +0300

    Update 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
-nb_keys = number_keys(a_dictionary)
-print("Number of keys: {:d}".format(nb_keys))
+def number_keys(a_dictionary):
+    return len(a_dictionary)
