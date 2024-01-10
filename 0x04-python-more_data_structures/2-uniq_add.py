4856d416114146f070dea448f29c16f3056cb969
Yared Asmelash <yaredasmelash00@gmail.com>
2024-01-11 00:10:48 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return sum(set(my_list))
+    # Create an empty set to store unique integers
+    unique_set = set()
+
+    # Add unique integers to the set
+    for i in my_list:
+        if i in unique_set:
+            continue
+        unique_set.add(i)
+
+    # Calculate and return the sum of unique integers
+    return sum(unique_set)
