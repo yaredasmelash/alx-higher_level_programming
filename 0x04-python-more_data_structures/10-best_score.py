efca1f123a6ba64cd043ec0e710213923bcfa78d
Yared Asmelash <yaredasmelash00@gmail.com>
2024-01-11 00:32:46 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
+best_key = None
+best_score = None
+
+for key, score in a_dictionary.items():
+    if best_score is None or score > best_score:
+        best_score = score
+        best_key = key
+
+return best_key
